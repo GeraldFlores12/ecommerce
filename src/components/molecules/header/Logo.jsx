3320f788b17fbccc3ef6e4388eecb2eb9742f9c7
Gerald Flores <floressgj2@gmail.com>
2022-12-26 10:29:58 -0600

    Se agreg router,links al menu y la configuración inicial de tailwind

---
+++ src/components/molecules/header/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <div>
+            <img src="https://gerald-flores-portafolio.netlify.app/images/profile.png" alt="Logo ecommerce" />
+        </div>
+    )
+}
+
+export default Logo
